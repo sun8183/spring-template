fb452714b6cec4bd30d2106596c3d3650b0ab78d
sun8183 <4sunskyhyun@gmail.com>
2026-04-09 01:56:10 +0900

    1. 에러처리 수정

--- src/main/java/com/taeyang/spring_template/common/exception/dto/FieldErrorResponse.java
+++ src/main/java/com/taeyang/spring_template/common/exception/dto/FieldErrorResponse.java
@@ -3,6 +3,7 @@ package com.taeyang.spring_template.common.exception.dto;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,11 +17,18 @@ public record FieldErrorResponse(
      * GlobalExceptionHandler에서 사용하기 편리하도록 정적 팩토리 메서드를 추가했습니다.
      */
     public static List<FieldErrorResponse> of(BindingResult bindingResult) {
-        return bindingResult.getFieldErrors().stream()
-                .map(error -> new FieldErrorResponse(
-                        error.getField(),
-                        error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
-                        error.getDefaultMessage()))
-                .collect(Collectors.toList());
+        return new ArrayList<>(bindingResult.getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField, // key: 필드명
+                        error -> new FieldErrorResponse(
+                                error.getField(),
+                                error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                                error.getDefaultMessage()
+                        ),
+                        (existing, replacement) -> existing // 중복 시 첫 번째 유지
+                ))
+                .values());
     }
+
+
 }
